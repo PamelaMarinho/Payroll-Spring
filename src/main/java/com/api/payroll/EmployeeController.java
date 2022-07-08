c5c7fbb689517342fc13bffd666f32c1e2cc89bf
pamelamarinho <pamelamarinhopm@hotmail.com>
2022-07-08 10:48:27 -0300

    first commit

---
+++ src/main/java/com/api/payroll/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.api.payroll;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    public EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/employee")
+    Employee newEmployee(@RequestBody Employee newEmployee){
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employee/{id}")
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id) //
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employee"));
+    }
+
+    @PutMapping("/employee/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employee/{id}")
+    void deleteEmployee(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/api/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.api.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
